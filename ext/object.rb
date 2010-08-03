92a5c5aa6acb65a03da15f99c6ecb57a5c4d5f44
Pierre Chapuis <pierre@moodstocks.com>
2010-08-03 11:20:27 +0200

    Doc: add comments to extensions in Object.
    Signed-off-by: Pierre Chapuis <pierre@moodstocks.com>

--- ext/object.rb
+++ ext/object.rb
@@ -1,8 +1,14 @@
 Object.class_eval do
+  
+  # Completely clone the object.
+  # No reference will be shared with the original.
   def deepcopy
     Marshal.load(Marshal.dump(self))
   end
+  
+  # Return true if the object is a boolean, false otherwise.
   def boolean?
     is_a?(TrueClass) || is_a?(FalseClass)
   end
+  
 end
