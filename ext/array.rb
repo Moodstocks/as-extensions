5bcf1938a021673e0b4edd225f94f397996a62ca
Pierre Chapuis <pierre@moodstocks.com>
2010-08-12 12:44:04 +0200

    New methods for Array.
    Array#tail, Array#head, Array#map_m, Array#map_f.
    
    Signed-off-by: Pierre Chapuis <pierre@moodstocks.com>

--- ext/array.rb
+++ ext/array.rb
@@ -1,5 +1,11 @@
 Array.class_eval do
 
+  alias :first :head
+  
+  def tail
+    self[1..-1]
+  end
+
   # Call ASE::deepcompact(self)
   def deepcompact
     ActiveSupport::Extension::deepcompact(self)
@@ -8,6 +14,16 @@ Array.class_eval do
   # Return a random element.
   def pick
     self[Kernel.rand(size)]
+  end
+  
+  # Map a method of members to themselves
+  def map_m(sym, *args)
+    self.map{ |x| x.send(sym.to_sym, *args) }
+  end
+  
+  # Map a function (ie. Method object) to members
+  def map_f(f, *args)
+    self.map{ |x| f.call(self, *args) }
   end
   
   # Boolean map: same as map but returns a boolean intersection of the resulting values.
