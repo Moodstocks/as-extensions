959789a03d205a5ac7c9cfd948571cb40f923672
Pierre Chapuis <catwell@archlinux.us>
2010-10-08 17:18:44 +0200

    Cosmetics: reorder methods

--- lib/as-extensions/test.rb
+++ lib/as-extensions/test.rb
@@ -31,26 +31,6 @@ module ActiveSupport module Extension module Test
   
   class << self
     
-    def debug(s)
-      puts s if CONFIG[:verbose]
-    end
-    
-    def stats(mute=nil)
-      CONFIG[:mute_stats] = mute if !mute.nil?
-      return if CONFIG[:mute_stats]
-      puts "=== TEST STATS ==="
-      puts "OK: #{CONFIG[:nb_ok]}"
-      puts "NOK: #{CONFIG[:nb_nok]}"
-    end
-  
-    def msg_stack(msg1=nil, msg2=nil)
-      if msg1
-        if msg2 then "#{msg1}\nERROR ==> #{msg2}"
-        else msg1 end
-      elsif msg2 then msg2
-      else nil end        
-    end
-  
     def assert(success=nil, msg=nil)
       if success
         CONFIG[:nb_ok] += 1
@@ -65,45 +45,41 @@ module ActiveSupport module Extension module Test
         return false
       end
     end
-    
-    def assert_fail(msg=nil)
-      assert false, msg
-    end
   
     def assert_boolean(b=nil, msg=nil)
       assert (b == true || b == false), msg_stack(msg, "Expected a boolean, found a #{b.class}.")
-    end
-  
-    def assert_true(b=nil, msg=nil)
-      assert b == true, msg
-    end
-    
-    def assert_false(b=nil, msg=nil)
-      assert b == false, msg
-    end
-    
-    def assert_nil(x=1, msg=nil)
-      assert x.nil?, msg
-    end
-    
-    def assert_not_nil(x=nil, msg=nil)
-      assert !x.nil?, msg
     end
     
     def assert_equal(a, b, msg=nil)
       assert a == b, msg_stack(msg, "#{b} found, expected #{a}")
     end
     
-    def assert_not_equal(a, b, msg=nil)
-      assert a != b, msg_stack(msg, "#{b} and #{a} are equal")
+    def assert_fail(msg=nil)
+      assert false, msg
+    end
+    
+    def assert_false(b=nil, msg=nil)
+      assert b == false, msg
+    end
+    
+    def assert_instance_of(k=nil, x=nil, msg=nil)
+      assert x.instance_of?(k), msg
     end
     
     def assert_kind_of(k=nil, x=nil, msg=nil)
       assert x.kind_of?(k), msg
     end
     
-    def assert_instance_of(k=nil, x=nil, msg=nil)
-      assert x.instance_of?(k), msg
+    def assert_nil(x=1, msg=nil)
+      assert x.nil?, msg
+    end
+    
+    def assert_not_equal(a, b, msg=nil)
+      assert a != b, msg_stack(msg, "#{b} and #{a} are equal")
+    end
+    
+    def assert_not_nil(x=nil, msg=nil)
+      assert !x.nil?, msg
     end
     
     def assert_raise(kind=Exception, msg=nil, &blk)
@@ -115,7 +91,15 @@ module ActiveSupport module Extension module Test
       end
       assert_fail msg_stack(msg, 'No exception raised.')
     end
-  
+    
+    def assert_true(b=nil, msg=nil)
+      assert b == true, msg
+    end
+    
+    def debug(s)
+      puts s if CONFIG[:verbose]
+    end
+    
     def isolate(msg=nil, &blk)
       fork do
         yield
@@ -125,7 +109,23 @@ module ActiveSupport module Extension module Test
       msg = msg.is_a?(::String) ? "Failure in isolation jail \"#{msg}\"" : nil
       assert_equal 0, $?.exitstatus, msg_stack(msg, "Process in isolation returned #{$?.exitstatus}")
     end
-  
+    
+    def msg_stack(msg1=nil, msg2=nil)
+      if msg1
+        if msg2 then "#{msg1}\nERROR ==> #{msg2}"
+        else msg1 end
+      elsif msg2 then msg2
+      else nil end
+    end
+    
+    def stats(mute=nil)
+      CONFIG[:mute_stats] = mute if !mute.nil?
+      return if CONFIG[:mute_stats]
+      puts "=== TEST STATS ==="
+      puts "OK: #{CONFIG[:nb_ok]}"
+      puts "NOK: #{CONFIG[:nb_nok]}"
+    end
+    
   end # class << self
   
 end end end
